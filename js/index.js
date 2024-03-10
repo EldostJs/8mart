7c535c9ebdf8dd99c74bcd85fd9e27633cfca43f
unknown <mirzeyev0308@gamil.com>
2024-03-10 18:18:57 +0400

    22

---
+++ js/index.js
@@ -0,0 +1,103 @@
+//------------1---------
+
+
+// let num = ["1","2","3","4","5","6","7","8","9","10"];
+
+// for (i = 1; i < 10; i++) {
+    
+// }
+// console.log(num);
+
+
+//------------2-3-------
+
+
+// var arr = [13, 23, 12, 45, 22, 48, 66, 100];
+// var evenNumbers = [];
+
+// arr.push(arr[arr.length - 1]);
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//         evenNumbers.push(arr[i]);
+//     }
+// }
+
+// console.log(arr);
+// console.log(evenNumbers);
+
+
+//-----------4-------------
+
+// var arr = [13, 23, 12, 45, 22, 48, 66, 100];
+
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     if (arr[i] % 2 === 0) {
+//         arr.splice(i, 1);
+//     }
+// }
+
+// console.log(arr);
+
+//----------5-----------
+
+// var arr = [23,56,4,78,5,63,45,210,56]
+
+// var delNum = 78;
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (delNum === arr[i]) {
+//         arr.splice(i,1)
+        
+//     }
+
+// }
+// console.log(arr);
+
+//---------6----------
+
+// var arr = [[1, 2], [3, 4], [5, 6]]
+
+// var num = []
+
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//         num.push(arr[i][j])        
+//     }    
+// }
+
+// console.log(arr);
+// console.log(num);
+
+//---------7----------
+
+// var num = "435389"
+
+// var arr = num.split("")
+
+// var sum = 0
+
+// for (let i = 0; i < arr.length; i++) {
+
+//     sum += parseInt(arr[i])
+// }
+
+
+// console.log(num);
+// console.log(arr);
+// console.log(sum);
+
+//---------8----------
+
+// var arr = [2, 45, 3, 67, 34, 567, 34, 345, 123] 
+
+// var maxNum = arr[0]
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > maxNum) {
+//         maxNum = arr[i]
+        
+//     }    
+// }
+// console.log(arr);
+// console.log(maxNum);
